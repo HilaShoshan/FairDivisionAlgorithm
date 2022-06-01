74de4ace82ca61ca674129518487e03815b39d32
Hila Shoshan <hilashoshan0605@gmail.com>
2022-06-01 12:58:36 +0300

    added edge colors for allocations graph in Main -- a common color does not always exists!

--- main.py
+++ main.py
@@ -3,18 +3,52 @@ from Instance import *
 
 
 def update_allocations_graph(key):
+    """
+    Add a new node that represent an allocation to the allocation_dict, if it does not exist yet. 
+    """
     global NODE_COUNT
-    if key not in allocations_dict:
+    if key not in allocation_dict:
         color_map.append('gray')
-        allocations_dict[key] = NODE_COUNT  # the value is also unique
+        allocation_dict[key] = NODE_COUNT  # the value is also unique
         allocations_graph.add_node(NODE_COUNT, name=NODE_COUNT)  # add a new node to the graph
         value = NODE_COUNT
         NODE_COUNT += 1
         flag = True  # the key is new in the dictionary (a new allocation)
     else:
-        value = allocations_dict[key]  # already exists in the allocations dictionary
+        value = allocation_dict[key]  # already exists in the allocations dictionary
         flag = False
     return value, flag
+
+
+def get_edge_color(envy_graph):
+    """
+    :param envy_graph: the envy graph of some allocation. 
+                        Has to be a DAG graph, since all allocations are w-maximal.
+    :return: an edge color determines by a topological ordering on the envy graph, and color_dict.
+    """
+    topological = list(nx.all_topological_sorts(envy_graph))
+    colors = []
+    for order in topological:
+        reversed_order = list(reversed(order))
+        color = color_dict[str(reversed_order)]
+        colors.append(color)
+    return colors
+
+
+def get_common(a, b):
+    """
+    Given two lists, returns an element which is common to both.
+    If there is no such element, returns 'k' (black color)
+    """
+    a_set = set(a)
+    b_set = set(b)
+     
+    # check length
+    intersection = a_set.intersection(b_set)
+    if len(intersection) > 0:
+        return(intersection[0])  # return the first common element
+    else:
+        return 'k'
 
 
 def algorithm1(I, stopOnEF1=True):
@@ -51,18 +85,23 @@ def algorithm1(I, stopOnEF1=True):
     flag = True
     while flag:  # while we discover new allocations
         if not A.is_EF1() or not stopOnEF1:
+            colors_before = get_edge_color(A.envy_graph)  # the possible colors for the node of the current allocation (before the exchange)
             A.exchange_pair(i, j, exchangeable_pair)
             print("exchange ", exchangeable_pair, " between ", i, " and ", j)
             print("\nupdated A = ", A.A)
             key = ' '.join(map(str, A.A))
             value, flag = update_allocations_graph(key)
-            # TODO: add edge color determines by a topological ordering on the envy graph
-            allocations_graph.add_edge(PREV_NODE, value)  # edge that spesifies we moved from the previous allocation to the current one
+            A.update_exchangeable_items()
+            colors_after = get_edge_color(A.envy_graph)  # possible colors after the exchange
+            edge_color = get_common(colors_before, colors_after)  # the common edge color before and after the exchange!
+            print("colors_before: ", colors_before)
+            print("colors_after: ", colors_after)
+            print("color: ", edge_color)
+            allocations_graph.add_edge(PREV_NODE, value, color=edge_color)  # edge that spesifies we moved from the previous allocation to the current one
             PREV_NODE = value  # update previous node for the next iteration
             if A.is_EF():  # no exchangeable pairs
                 print('\n', A.A, " is EF!\n")
                 return A
-            A.update_exchangeable_items()
             i, j, exchangeable_pair = A.get_max_r_pair()
         if A.is_EF1(): 
             color_map[-1] = 'pink'
@@ -87,17 +126,17 @@ n = 3
 I = Instance(utilities, capacities, n)
 
 # this color dictionary is specific for 3 agents case
-colors_dict = {
-    '1>2>3' : 'red',
-    '1>3>2' : 'blue',
-    '2>1>3' : 'yellow',
-    '2>3>1' : 'black',
-    '3>1>2' : 'purple',
-    '3>2>1' : 'green'
+color_dict = {
+    '[0, 1, 2]' : 'r',   # 0>1>2 --> red
+    '[0, 2, 1]' : 'b',   # 0>2>1 --> blue
+    '[1, 0, 2]' : 'y',   # 1>0>2 --> yellow
+    '[1, 2, 0]' : 'g',   # 1>2>0 --> green
+    '[2, 0, 1]' : 'm',   # 2>0>1 --> magenta 
+    '[2, 1, 0]' : 'c'    # 2>1>0 --> cyan
 }
 
 allocations_graph = nx.Graph()
-allocations_dict = {} 
+allocation_dict = {} 
 NODE_COUNT = 0
 PREV_NODE = None
 color_map = []
@@ -105,10 +144,12 @@ color_map = []
 stopOnEF1 = True
 algorithm1(I, stopOnEF1)
 
-nx.draw(allocations_graph, node_size=1000, font_size=8, font_weight='bold', node_color=color_map, with_labels=True)
+edges = allocations_graph.edges()
+colors = [allocations_graph[u][v]['color'] for u,v in edges]
+nx.draw(allocations_graph, edgelist=edges, edge_color=colors, node_size=1000, font_size=8, font_weight='bold', node_color=color_map, with_labels=True)
 plt.savefig("plots/AllocationsGraph.png", format="PNG")
 plt.close()
 
 print("_________________________allocation dictionary_________________________")
-print(allocations_dict)
+print(allocation_dict)
 print("_______________________________________________________________________")
